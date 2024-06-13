d4239acb49408d2a6d6f02726b11866606f20a3d
ggeywgygcy <wasilij18bus@gmail.com>
2024-06-13 15:56:00 +0300

    надо доделать

---
+++ converter.js
@@ -0,0 +1,51 @@
+const dollBtn = document.getElementById('doll')
+const chinBtn = document.getElementById('chin')
+const yaponBtn = document.getElementById('yapon')
+const tenBtn = document.getElementById('list')
+const val = document.getElementById('exampleInputEmail1')
+const group = document.getElementById('group')
+
+const number = val.ariaValueMax
+
+dollBtn.onclick = function(){
+    let res = Number(number * 60
+    //console.log(res)
+}
+
+console.log(typeof res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chinBtn.onclick = function(){
+//     res = Number(numb * 20)
+// }
+/*yaponBtn.onclick = function()
+    res = numb * 30
+}
+
+tenBtn.onclick = function(){
+    res = numb * 40
+}*/
