d650c9ddf618c745159ade9e931f9a181b71d8fb
ggeywgygcy <wasilij18bus@gmail.com>
2024-06-14 10:49:07 +0300

    нужно чуточку оптимизировать код и добавить еще функций

--- converter.js
+++ converter.js
@@ -5,19 +5,23 @@ const tenBtn = document.getElementById('list')
 const val = document.getElementById('exampleInputEmail1')
 const group = document.getElementById('group')
 
-const number = val.ariaValueMax
 
-dollBtn.onclick = function(){
-    let res = Number(number * 60
-    //console.log(res)
-}
-
-console.log(typeof res)
-
-
-
-
-
+dollBtn.onclick = function(res){
+    res = val.value * 88,21
+    group.textContent = res
+}    
+chinBtn.onclick = function(res){
+    res = val.value * 12.04
+    group.textContent = res
+}    
+yaponBtn.onclick = function(res){
+    res = val.value * 0.562551
+    group.textContent = res
+}    
+tenBtn.onclick = function(res){
+    res = val.value * 0.19525
+    group.textContent = res
+}    
 
 
 
